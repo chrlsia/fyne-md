a8af355db5dd69f5fd285c59fb0f65d6f3b47ac2
Chris Siannas <xrsia@otenet.gr>
2022-07-04 17:35:47 +0300

    Getting started with md

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -16,13 +18,25 @@ var cfg config
 
 func main(){
 	// create a fyne app
-
+	a:=app.New()
 	// create a window for the app
-
+	win:=a.NewWindow("Markdown")
 	// get the user interface
-
+	edit,preview:= cfg.makeUI()
 	// set the content of the window
-
+	win.SetContent(container.NewHSplit(edit,preview))
 	// show window and run app
+	win.Resize(fyne.Size{Width:800, Height:800})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
 
+func (app *config) makeUI() (*widget.Entry,*widget.RichText){
+	edit:=widget.NewMultiLineEntry()
+	preview:=widget.NewRichTextFromMarkdown("")
+	app.EditWidget=edit
+	app.PreviewWidger=preview
+
+	edit.OnChanged = preview.ParseMarkdown
+	return edit,preview
 }
