a18c2bb336d209556cb49b9d99f58d628df7b110
Chris Siannas <xrsia@otenet.gr>
2022-07-04 12:24:48 +0300

    config struct and steps

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+type config struct{
+	EditWidget *widget.Entry
+	PreviewWidger *widget.RichText
+	CurrentFile fyne.URI
+	SaveMenuItem fyne.MenuItem
+}
+
+var cfg config
+
 func main(){
-	
+	// create a fyne app
+
+	// create a window for the app
+
+	// get the user interface
+
+	// set the content of the window
+
+	// show window and run app
+
 }
